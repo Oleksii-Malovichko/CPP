f201f8e53b6613c01ab6fba1f8a7bad1f679d58a
Oleksii Malovichko <omalovic@2-C-8.42heilbronn.de>
2025-05-19 16:09:03 +0200

    changed main in ex00

--- C02/ex00/main.cpp
+++ C02/ex00/main.cpp
@@ -1,33 +1,33 @@
 #include "Fixed.hpp"
 
-// int main( void )
-// {
-// 	Fixed a;
-// 	Fixed b( a );
-// 	Fixed c;
-// 	c = b;
-// 	std::cout << a.getRawBits() << std::endl;
-// 	std::cout << b.getRawBits() << std::endl;
-// 	std::cout << c.getRawBits() << std::endl;
-// 	return 0;
-// }
-
-int main( void ) {
-	
+int main( void )
+{
 	Fixed a;
-	Fixed b(a);
+	Fixed b( a );
 	Fixed c;
-
-	a.setRawBits(50);
-	b.setRawBits(85);
-	c.setRawBits(6);
-
-	std::cout << c.getRawBits() << std::endl;
 	c = b;
-	
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
-
 	return 0;
 }
+
+// int main( void ) {
+	
+// 	Fixed a;
+// 	Fixed b(a);
+// 	Fixed c;
+
+// 	a.setRawBits(25);
+// 	b.setRawBits(30);
+// 	c.setRawBits(3);
+
+// 	std::cout << c.getRawBits() << std::endl;
+// 	c = b;
+	
+// 	std::cout << a.getRawBits() << std::endl;
+// 	std::cout << b.getRawBits() << std::endl;
+// 	std::cout << c.getRawBits() << std::endl;
+
+// 	return 0;
+// }
