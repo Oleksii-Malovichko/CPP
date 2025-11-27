f41aa657b0c21ff48d4d07a2986eb27b52845a15
Oleksii Malovichko <omalovic@3-E-17.42heilbronn.de>
2025-11-27 14:22:16 +0100

    started cpp7

---
+++ train_cpp/cpp7/ex00/whatever.hpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T temp = a;
+
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+const T& min(T &a, T &b)
+{
+	if (a < b)
+		return a;
+	else
+		return b;
+}
+
+template <typename T>
+const T& max(T &a, T &b)
+{
+	if (a > b)
+		return a;
+	else
+		return b;
+}
