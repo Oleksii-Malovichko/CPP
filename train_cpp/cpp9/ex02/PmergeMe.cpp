6f494126a2f510e00cc206bb5a732ebd800ac357
alex <alex@dev-kate-log>
2025-12-12 16:17:45 +0100

    seems that for vector sorting using alghorithm ford-johnson is working!

--- train_cpp/cpp9/ex02/PmergeMe.cpp
+++ train_cpp/cpp9/ex02/PmergeMe.cpp
@@ -18,11 +18,51 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &other)
 
 PmergeMe::~PmergeMe() {}
 
+std::vector<size_t> buildJacobsthalIndices(size_t n)
+{
+    std::vector<size_t> order;
+    if (n == 0) return order;
+
+    // Jacobsthal numbers: 1, 3, 5, 11, 21, ...
+    std::vector<size_t> jac;
+    {
+        size_t a = 1, b = 3;
+        if (a < n) jac.push_back(a);
+        if (b < n) jac.push_back(b);
+        while (true) {
+            size_t c = b + 2 * a;
+            if (c >= n) break;
+            jac.push_back(c);
+            a = b;
+            b = c;
+        }
+    }
+
+    size_t prev = 0;
+
+    for (size_t j : jac) {
+        // Insert the Jacobsthal index
+        order.push_back(j);
+
+        // Insert backwards from j-1 down to prev
+        for (size_t i = j; i-- > prev;)
+            order.push_back(i);
+
+        prev = j + 1;
+    }
+
+    // Tail: (n-1) down to prev
+    for (size_t i = n; i-- > prev;)
+        order.push_back(i);
+
+    return order;
+}
+
 void PmergeMe::mergeInsertSort(std::vector<unsigned int> &vec)
 {
 	if (vec.size() <= 1)
 		return;
-
+	// std::cout << "vec size: " << vec.size() << std::endl;
 	std::vector<unsigned int> bigs;
 	std::vector<unsigned int> smalls;
 	// separete to the pairs
@@ -45,10 +85,25 @@ void PmergeMe::mergeInsertSort(std::vector<unsigned int> &vec)
 	// sorting the bigs using recursion
 	mergeInsertSort(bigs);
 	// select smalls to the sorted bigs arr
-	for (unsigned int s : smalls)
+	// std::cout << "Smalls size: " << smalls.size() << std::endl;
+	std::vector<size_t> order = buildJacobsthalIndices(smalls.size());
+	// std::cout << "Smalls order size: " << order.size() << std::endl;
+	// for (size_t i = 0; i < order.size(); i++)
+	// {
+	// 	std::cout << order[i] << " ";
+	// }
+	// std::cout << std::endl;
+	// for (unsigned int s : smalls)
+	// {
+	// 	auto pos = std::lower_bound(bigs.begin(), bigs.end(), s);
+	// 	bigs.insert(pos, s);
+	// }
+
+	for (size_t idx : order)
 	{
-		auto pos = std::lower_bound(bigs.begin(), bigs.end(), s);
-		bigs.insert(pos, s);
+		unsigned int value = smalls[idx];
+		auto pos = std::lower_bound(bigs.begin(), bigs.end(), value);
+		bigs.insert(pos, value);
 	}
 	vec = bigs;
 }
--- train_cpp/cpp9/ex02/main.cpp
+++ train_cpp/cpp9/ex02/main.cpp
@@ -1,16 +1,39 @@
 #include "PmergeMe.hpp"
 
-void isSorted(std::vector<unsigned int> &vec)
+bool isSorted(std::vector<unsigned int> &vec)
 {
 	for (size_t i = 0; i + 1 < vec.size(); i++)
 	{
 		if (vec[i] > vec[i + 1])
 		{
-			std::cout << "NOT SORTED " << i << std::endl;
-			return;
+			return 0;
 		}
 	}
-	std::cout << "SORTED" << std::endl;
+	return 1;
+}
+
+bool checkLostElements(std::vector<unsigned int> vec, std::vector<unsigned int> v_sorted)
+{
+	bool found = 0;
+	if (vec.size() != v_sorted.size())
+		return 0;
+	for (size_t i = 0; i < vec.size() ; i++)
+	{
+		size_t j = 0;
+		found = 0;
+		while (j < v_sorted.size())
+		{
+			if (vec[i] == v_sorted[j])
+			{
+				found = 1;
+				break;
+			}
+			j++;
+		}
+		if (!found)
+			return 0;
+	}
+	return 1;
 }
 
 int main(int argc, char **argv)
@@ -48,11 +71,11 @@ int main(int argc, char **argv)
 	std::cout << "Before:" << std::endl;
 	for (auto it = lst.begin(); it != lst.end(); it++)
 	{
-		if (lst.size() > 5 && i == 4)
-		{
-			std::cout << "[...]";
-			break;
-		}
+		// if (lst.size() > 5 && i == 4)
+		// {
+		// 	std::cout << "[...]";
+		// 	break;
+		// }
 		std::cout << *it << " ";
 		i++;
 	}
@@ -61,6 +84,7 @@ int main(int argc, char **argv)
 	PmergeMe merge;
 	std::cout << "After:" << std::endl;
 	merge.mergeInsertSort(vec);
+
 	i = 0;
 	std::vector<unsigned int> v_sorted;
 	for (auto it = vec.begin(); it != vec.end(); it++)
@@ -75,6 +99,13 @@ int main(int argc, char **argv)
 		i++;
 	}
 	std::cout << std::endl;
-	isSorted(v_sorted); // this func is only to show the evaluator that the vector is really sorted
+	if (isSorted(v_sorted) && vec.size() == v_sorted.size())
+		std::cout << "SORTED [OK]" << std::endl;
+	else
+		std::cout << "NOT SORTED [ERROR]" << std::endl;
+	if (checkLostElements(vec, v_sorted))
+		std::cout << "NO LOSTED [OK]" << std::endl;
+	else
+		std::cout << "LOSTED ELEMENTS [ERROR]" << std::endl;
 	return 0;
 }
